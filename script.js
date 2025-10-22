6b9cb1965cc80e8da2d18d8fb1b61f330199b1c2
Shafie <198397797+Shafiekh@users.noreply.github.com>
2025-10-22 22:18:41 +0330

    edit script
    script for play ready

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("hello shafo");
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+//console.log(getRandomInt(3));
+
+function getComputerChoice() {
+	let result = getRandomInt(3);
+	//console.log(result);
+	if (result === 0 ) {
+		console.log("rock");
+	} else if (result === 1 ) {
+		console.log("paper");
+	} else {
+		console.log("scissors");
+	}
+}
+
+getComputerChoice();
